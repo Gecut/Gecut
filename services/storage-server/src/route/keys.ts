9f2539e3bffe189d35696cf553906116e9a4d221
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-02 23:50:54 +0330

    docs(services|storage-server): jsdoc

--- services/storage-server/src/route/keys.ts
+++ services/storage-server/src/route/keys.ts
@@ -9,6 +9,18 @@ import type {
 
 nanoServer.route('GET', '/keys', getStorageKeys);
 
+/**
+ * It requires a token, requires a query parameter
+ * called `storage` and returns the keys of the storage
+ * engine with that name
+ *
+ * @param {AlwatrConnection} connection - AlwatrConnection -
+ * this is the connection object that is
+ * passed to the service. It contains the request
+ * , response, and other useful information.
+ *
+ * @returns An object with a boolean property called ok and a data property.
+ */
 function getStorageKeys(connection: AlwatrConnection): AlwatrServiceResponse {
   logger.logMethod('getStorageKeys');
 
