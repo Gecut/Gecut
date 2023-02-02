24fea1b1f3d22c9970c03c6d1aaca5b80682c194
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-02 23:37:20 +0330

    lint: prettier

--- services/storage-server/src/route/has.ts
+++ services/storage-server/src/route/has.ts
@@ -3,26 +3,26 @@ import {nanoServer} from '../lib/nano-server.js';
 import {storageProvider} from '../lib/storage-provider.js';
 
 import type {
-	AlwatrConnection,
-	AlwatrServiceResponse,
+  AlwatrConnection,
+  AlwatrServiceResponse,
 } from '@alwatr/nano-server';
 
 nanoServer.route('GET', '/has', has);
 
 function has(connection: AlwatrConnection): AlwatrServiceResponse {
-	logger.logMethod('has');
+  logger.logMethod('has');
 
-	connection.requireToken(config.nanoServer.accessToken);
+  connection.requireToken(config.nanoServer.accessToken);
 
-	const params = connection.requireQueryParams<{storage: string; id: string}>({
-		storage: 'string',
-		id: 'string',
-	});
+  const params = connection.requireQueryParams<{storage: string; id: string}>({
+    storage: 'string',
+    id: 'string',
+  });
 
-	const storageEngine = storageProvider.get({name: params.storage});
+  const storageEngine = storageProvider.get({name: params.storage});
 
-	return {
-		ok: true,
-		data: {has: storageEngine.has(params.id)},
-	};
+  return {
+    ok: true,
+    data: {has: storageEngine.has(params.id)},
+  };
 }
