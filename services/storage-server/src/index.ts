7c048bbc1ba276f5db47cca36b42142c04c253d9
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-02 22:16:51 +0330

    feat(services/storage-server): new nano service

---
+++ services/storage-server/src/index.ts
@@ -0,0 +1,10 @@
+import {logger} from './config.js';
+
+import './route/get.js';
+import './route/has.js';
+import './route/patch.js';
+import './route/delete.js';
+import './route/keys.js';
+import './route/storage.js';
+
+logger.logOther('⚞ Gecut Storage Server ⚟');
