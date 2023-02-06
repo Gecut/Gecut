5429c0cfa93b05d815e6efd252ec6734b951b5fc
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-06 21:45:02 +0330

    feat(server|agah-api): index

--- server/agah-api/src/index.ts
+++ server/agah-api/src/index.ts
@@ -1,6 +1,10 @@
 import './routes/home.js';
 import './routes/sans/get.js';
 import './routes/sans/post.js';
+import './routes/user/get.js';
+import './routes/auth/sign-up.js';
+import './routes/user/put.js';
+import './routes/auth/sign-in.js';
 import {logger} from './config.js';
 
 logger.logOther('⚞ Gecut Agah Api ⚟');
