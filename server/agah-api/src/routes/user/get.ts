4cb18742812eb565a503f60665963cb3225831c6
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-06 21:43:13 +0330

    feat(server|agah-api|user): get & edit route

---
+++ server/agah-api/src/routes/user/get.ts
@@ -0,0 +1,66 @@
+import {logger} from '../../config.js';
+import {nanoServer} from '../../libs/nano-server.js';
+import {requireUserVerify} from '../../libs/require-user-verify.js';
+import {storageClient} from '../../libs/storage.js';
+
+import type {
+  AlwatrConnection,
+  AlwatrServiceResponse,
+} from '@alwatr/nano-server';
+import type {UserInterface, UserResponseData} from '../../types/user.js';
+
+nanoServer.route('GET', '/user', getUserData);
+/**
+ * It gets the list of users from the storage service
+ *
+ * @param {AlwatrConnection} connection - AlwatrConnection
+ *
+ * @returns An array of UserResponseData objects.
+ */
+async function getUserData(
+  connection: AlwatrConnection,
+): Promise<AlwatrServiceResponse<UserResponseData>> {
+  logger.logMethod('getUserData');
+
+  const param = connection.requireQueryParams<{id: string}>({id: 'string'});
+  const token = connection.getToken();
+  const userVerifyResult = await requireUserVerify(param.id, token);
+
+  if (userVerifyResult.ok === false) {
+    return userVerifyResult;
+  }
+
+  try {
+    const group = await storageClient.getStorage<UserInterface>('user');
+
+    group.data = Object.fromEntries(
+      Object.values(group.data)
+        .filter((user) => user.groupId === userVerifyResult.data.user.groupId)
+        .map((user: Partial<UserInterface>) => {
+          delete user.auth;
+          return [user.id, user as Omit<UserInterface, 'auth'>];
+        }),
+    );
+
+    return {
+      ok: true,
+      data: {
+        ...userVerifyResult.data.user,
+        group: group.data,
+      },
+    };
+  } catch (_err) {
+    const err = _err as Error;
+    logger.error('editUser', err.message || 'storage_error', err);
+    return {
+      ok: false,
+      statusCode: 500,
+      errorCode: 'storage_error',
+      meta: {
+        name: err.name,
+        message: err.message,
+        cause: err.cause,
+      },
+    };
+  }
+}
---
+++ server/agah-api/src/routes/user/put.ts
@@ -0,0 +1,77 @@
+import {logger} from '../../config.js';
+import {nanoServer} from '../../libs/nano-server.js';
+import {storageClient} from '../../libs/storage.js';
+import {requireUserVerify} from '../../libs/require-user-verify.js';
+
+import type {
+  AlwatrConnection,
+  AlwatrServiceResponse,
+} from '@alwatr/nano-server';
+import type {SansInterface} from '../../types/sans.js';
+import type {UserInterface} from '../../types/user.js';
+
+nanoServer.route('PUT', '/user', editUser);
+/**
+ * `editUser` edits a user
+ *
+ * @param {AlwatrConnection} connection - AlwatrConnection
+ *
+ * @returns A function that returns a promise that resolves
+ *  to an AlwatrServiceResponse.
+ */
+async function editUser(
+  connection: AlwatrConnection,
+): Promise<AlwatrServiceResponse> {
+  logger.logMethod('editUser');
+
+  const jsonBody = await connection.requireJsonBody<Partial<UserInterface>>();
+  const token = connection.getToken();
+
+  if (jsonBody.id == null) {
+    return {
+      ok: false,
+      statusCode: 404,
+      errorCode: 'user_id_required',
+    };
+  }
+
+  const userVerifyResult = await requireUserVerify(jsonBody.id, token);
+  if (userVerifyResult.ok === false) {
+    return userVerifyResult;
+  }
+
+  try {
+    const sansList = await storageClient.getStorage<SansInterface>('sans');
+
+    const user: UserInterface = {
+      ...userVerifyResult.data.user,
+      ...jsonBody,
+    };
+
+    if (jsonBody.sansCode != null && sansList.data[user.sansCode].id == null) {
+      return {
+        ok: false,
+        statusCode: 404,
+        errorCode: 'sans_code_not_found',
+      };
+    }
+
+    return {
+      ok: true,
+      data: await storageClient.set(user, 'user'),
+    };
+  } catch (_err) {
+    const err = _err as Error;
+    logger.error('editUser', err.message || 'storage_error', err);
+    return {
+      ok: false,
+      statusCode: 500,
+      errorCode: 'storage_error',
+      meta: {
+        name: err.name,
+        message: err.message,
+        cause: err.cause,
+      },
+    };
+  }
+}
