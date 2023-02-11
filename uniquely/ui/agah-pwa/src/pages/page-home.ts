0a340667cef18e3c2c27077fe1d83840860df18a
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-12 01:34:12 +0330

    feat(ui|agah-pwa|page): new pages

--- uniquely/ui/agah-pwa/src/pages/page-home.ts
+++ uniquely/ui/agah-pwa/src/pages/page-home.ts
@@ -3,14 +3,27 @@ import {
   css,
   html,
   customElement,
-  LocalizeMixin as localizeMixin,
+  unsafeCSS,
 } from '@alwatr/element';
+import userAddIcon from '@gecut/iconsax-cdn/broken/user-add?raw';
+import clockIcon from '@gecut/iconsax-cdn/broken/clock?raw';
+import ticketIcon from '@gecut/iconsax-cdn/broken/ticket-2?raw';
+import callIcon from '@gecut/iconsax-cdn/broken/call-calling?raw';
+import logoImage from '/images/logo.png?inline';
+import iconImage from '/images/icon.png?inline';
+import baseElementStyle from '../styles/element.css?inline';
+import config from '../config.js';
+
+import '@gecut/ui-kit/icon/icon.js';
+
+import '../components/button/button';
 
 import type {LitRenderType} from '../types/lit-render.js';
 
 @customElement('page-home')
-export class PageHome extends localizeMixin(AlwatrDummyElement) {
+export class PageHome extends AlwatrDummyElement {
   static override styles = [
+    unsafeCSS(baseElementStyle),
     css`
       :host {
         display: flex;
@@ -20,13 +33,97 @@ export class PageHome extends localizeMixin(AlwatrDummyElement) {
 
         height: 100%;
 
+        gap: calc(4 * var(--sys-spacing-track));
+
         padding: calc(3 * var(--sys-spacing-track));
+      }
+
+      .buttons {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        gap: var(--sys-spacing-track);
+      }
+      .buttons gecut-button {
+        width: 100%;
+      }
+
+      .row {
+        display: flex;
+        width: 100%;
+        gap: var(--sys-spacing-track);
+      }
+      .row gecut-button {
+        flex-grow: 1;
+      }
+
+      .logo {
+        margin-top: auto;
+        width: 30vw;
+        max-width: calc(20 * var(--sys-spacing-track));
+      }
+
+      .version {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        direction: ltr;
+        gap: var(--sys-spacing-track);
+        width: 100%;
+        color: var(--sys-color-surface);
+        opacity: 40%;
+        margin-top: auto;
+
+        font-weight: 200;
+        font-family: var(--sys-typescale-title-large-font-family-name);
+        font-size: var(--sys-typescale-title-large-font-size);
+        letter-spacing: 2px;
+        line-height: var(--sys-typescale-title-large-line-height);
+      }
+      .version img {
+        opacity: 40%;
+        width: calc(7 * var(--sys-spacing-track));
       }
     `,
   ];
 
   override render(): LitRenderType {
-    return html``;
+    return html`
+      <img .src=${logoImage} class="logo" alt="logo" />
+      <div class="buttons">
+        <gecut-button href="/sign-up-details">
+          <gecut-icon slot="icon" .svgContent=${userAddIcon}></gecut-icon>
+
+          <span>ثبت نام</span>
+        </gecut-button>
+
+        <div class="row">
+          <gecut-button icon-button background="neutral">
+            <gecut-icon slot="icon" .svgContent=${clockIcon}></gecut-icon>
+
+            <span>سانس اجرا</span>
+          </gecut-button>
+
+          <gecut-button icon-button background="neutral" text="tertiary">
+            <gecut-icon slot="icon" .svgContent=${ticketIcon}></gecut-icon>
+
+            <span>پیگیری بلیط</span>
+          </gecut-button>
+        </div>
+
+        <gecut-button background="neutral">
+          <gecut-icon slot="icon" .svgContent=${callIcon}></gecut-icon>
+
+          <span>پشتیبانی</span>
+        </gecut-button>
+      </div>
+      <div class="version">
+        <img .src=${iconImage} alt="icon" />
+        <span>v${config.version}</span>
+      </div>
+    `;
   }
 }
 
---
+++ uniquely/ui/agah-pwa/src/pages/page-sign-up.ts
@@ -0,0 +1,254 @@
+import {
+  AlwatrDummyElement,
+  css,
+  html,
+  customElement,
+  unsafeCSS,
+  state,
+  unsafeHTML,
+} from '@alwatr/element';
+import {styleMap} from 'lit/directives/style-map.js';
+import {classMap} from 'lit/directives/class-map.js';
+import baseElementStyle from '../styles/element.css?inline';
+import {validateData} from '../utilities/validate.js';
+
+import '../components/button/button';
+import '../components/sign-up-slider/nickname';
+import '../components/sign-up-slider/age';
+import '../components/sign-up-slider/teammate';
+import '../components/sign-up-slider/gender';
+import '../components/sign-up-slider/sans';
+import '../components/sign-up-slider/phone';
+
+import type {PropertyValues} from '@alwatr/element';
+import type {UserInterface} from '../types/user.js';
+import type {LitRenderType} from '../types/lit-render.js';
+import type {Slider} from '../components/sign-up-slider/slider.js';
+
+@customElement('page-sign-up')
+export class PageSignUp extends AlwatrDummyElement {
+  static override styles = [
+    unsafeCSS(baseElementStyle),
+    css`
+      :host {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        height: 100%;
+
+        gap: calc(2 * var(--sys-spacing-track));
+      }
+
+      .slider {
+        display: flex;
+        overflow-x: hidden;
+        flex-grow: 1;
+        width: 100%;
+        direction: ltr;
+        overscroll-behavior: contain;
+        overflow: visible;
+      }
+      .slides {
+        display: flex;
+        flex-grow: 1;
+        width: max-content;
+        transition-property: transform;
+        transition-duration: var(--sys-motion-duration-medium);
+        transition-timing-function: var(--sys-motion-easing-in-out);
+      }
+      .slides > * {
+        direction: rtl;
+        flex-shrink: 0;
+        width: 100vw;
+        max-width: var(--_max-mobile-size);
+      }
+
+      footer {
+        direction: ltr;
+        display: flex;
+        width: 100%;
+        gap: var(--sys-spacing-track);
+        margin-top: calc(2 * var(--sys-spacing-track));
+        padding: calc(3 * var(--sys-spacing-track));
+      }
+      footer gecut-button.first {
+        border-top-right-radius: 0;
+      }
+      footer gecut-button.last {
+        border-top-left-radius: 0;
+      }
+      footer .progress {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 1;
+        gap: calc(0.5 * var(--sys-spacing-track));
+        padding: var(--sys-spacing-track);
+      }
+      footer .progress span {
+        display: flex;
+        flex-shrink: 0;
+        width: calc(1.4 * var(--sys-spacing-track));
+        height: calc(1.4 * var(--sys-spacing-track));
+        box-shadow: inset 0 0 calc(0.25 * var(--sys-spacing-track))
+          var(--sys-color-surface);
+        background-color: var(--sys-color-on-surface-variant);
+        border-radius: var(--sys-radius-large);
+        transition-property: box-shadow;
+        transition-duration: var(--sys-motion-duration-medium);
+        transition-timing-function: var(--sys-motion-easing-in-out);
+      }
+      footer .progress span.active {
+        box-shadow: inset 0 0 calc(2 * var(--sys-spacing-track))
+          var(--sys-color-surface);
+      }
+    `,
+  ];
+
+  @state()
+  private slideIndex = 0;
+
+  @state()
+  private data: Partial<UserInterface> = {};
+
+  private slides = [
+    'nickname-slider#nickname-slider',
+    'gender-slider#gender-slider',
+    'age-slider#age-slider',
+    'phone-slider#phone-slider',
+    'teammate-slider#teammate-slider',
+    'sans-slider#sans-slider',
+    'nickname-slider#fake-slider',
+  ];
+
+  override render(): LitRenderType {
+    const thisWidth = this.getBoundingClientRect().width;
+    const slidesStyle = styleMap({
+      transform: `translateX(${-1 * thisWidth * this.slideIndex + 'px'})`,
+    });
+    const slidesTemplate = this.slides.map((slide) => {
+      const slideDetail = slide.split('#');
+
+      slide = slideDetail[0];
+
+      return unsafeHTML(`<${slide} id=${slideDetail[1]}></${slide}>`);
+    });
+    const progressItemsTemplate = this.slides.map((_slide, index) => {
+      const progressItemClass = classMap({
+        active: index <= this.slideIndex,
+      });
+
+      return html`<span class=${progressItemClass}></span>`;
+    });
+
+    return html`
+      <div class="slider">
+        <div class="slides" style=${slidesStyle}>${slidesTemplate}</div>
+      </div>
+
+      <footer>
+        <gecut-button
+          class="first"
+          background="neutral"
+          ?disabled=${!this.canPerv}
+          @click=${this.pervSlide}
+        >
+          <span>قبلی</span>
+        </gecut-button>
+
+        <div class="progress">${progressItemsTemplate}</div>
+
+        <gecut-button
+          class="last"
+          ?disabled=${!this.canNext}
+          @click=${this.nextSlide}
+        >
+          <span>بعدی</span>
+        </gecut-button>
+      </footer>
+    `;
+  }
+
+  override updated(changedProperties: PropertyValues<this>): void {
+    super.updated(changedProperties);
+
+    for (const slide of this.renderRoot.querySelectorAll<Slider>(
+      this.slides.join(','),
+    )) {
+      if (slide != null) {
+        slide.active = false;
+        slide.data = {
+          ...slide.data,
+          ...this.data,
+        };
+
+        slide.removeEventListener('change', () => this.requestUpdate());
+      }
+    }
+
+    const slide = this.renderRoot.querySelector<Slider>(
+      this.slides[this.slideIndex],
+    );
+
+    if (slide != null) {
+      slide.active = true;
+      slide.addEventListener('change', () => this.requestUpdate());
+    }
+  }
+
+  private async nextSlide(): Promise<void> {
+    if (this.canNext) {
+      this.slideIndex++;
+    }
+  }
+  private pervSlide(): void {
+    if (this.canPerv) {
+      this.slideIndex--;
+    }
+  }
+
+  private get canNext(): boolean {
+    const slide = this.renderRoot.querySelector<Slider>(
+      this.slides[this.slideIndex],
+    );
+
+    if (slide == null) return false;
+
+    const dataValidate = Object.keys(slide.data)
+      .map((dataKey) => {
+        const key = dataKey as keyof UserInterface;
+
+        return validateData(key, slide.data[key]);
+      })
+      .reduce((perv, curr) => perv && curr, true);
+
+    if (dataValidate) {
+      this.data = {
+        ...this.data,
+        ...slide.data,
+      };
+    }
+
+    if (this.slideIndex + 1 < this.slides.length && dataValidate) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private get canPerv(): boolean {
+    if (this.slideIndex > 0) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'page-sign-up': PageSignUp;
+  }
+}
