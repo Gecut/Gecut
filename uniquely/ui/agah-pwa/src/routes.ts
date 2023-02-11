e54f105e41547db92077f6e31a598a544c55be7c
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-12 01:34:24 +0330

    feat(ui|agah-pwa): new routes

--- uniquely/ui/agah-pwa/src/routes.ts
+++ uniquely/ui/agah-pwa/src/routes.ts
@@ -1,17 +1,27 @@
 import {html} from '@alwatr/element';
 
 import './pages/page-home';
+import './pages/page-sign-up';
+import './pages/page-sign-up-details';
 import './components/color-palette';
 
 import type {Routes} from './types/route.js';
 
 const routes: Routes = {
-  home: {
-    icon: 'home',
+  'home': {
+    icon: null,
     render: () => html`<page-home></page-home>`,
   },
-  colors: {
-    icon: 'color',
+  'sign-up': {
+    icon: null,
+    render: () => html`<page-sign-up></page-sign-up>`,
+  },
+  'sign-up-details': {
+    icon: null,
+    render: () => html`<page-sign-up-details></page-sign-up-details>`,
+  },
+  'colors': {
+    icon: null,
     render: () => html`<color-palette></color-palette>`,
   },
 };
