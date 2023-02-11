d973f58a409d8e2c44c7f09edd25940c14d6d7b7
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-12 01:31:08 +0330

    feat(ui|agah-pwa|gender-card): new ui component

---
+++ uniquely/ui/agah-pwa/src/components/gender/gender-card.ts
@@ -0,0 +1,90 @@
+import {
+  AlwatrDummyElement,
+  css,
+  customElement,
+  html,
+  nothing,
+  property,
+  unsafeCSS,
+} from '@alwatr/element';
+import baseElementStyle from '../../styles/element.css?inline';
+import surfaceStyle from '../../styles/surface.css?inline';
+import maleIcon from '/icons/gender/man-outline.svg?raw';
+import femaleIcon from '/icons/gender/woman-outline.svg?raw';
+
+import type {Gender} from '../../types/user.js';
+import type {LitRenderType} from '../../types/lit-render.js';
+
+@customElement('gecut-gender-card')
+export class GenderCard extends AlwatrDummyElement {
+  static override styles = [
+    unsafeCSS(baseElementStyle),
+    css`
+      :host {
+        --_surface-color-on: var(--sys-color-primary-container-hsl);
+        --_surface-color-bg: var(--sys-color-on-primary-container-hsl);
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 0;
+        border-radius: var(--sys-radius-medium);
+        user-select: none;
+        overflow: hidden;
+        cursor: pointer;
+
+        color: hsl(var(--_surface-color-on));
+        background-color: hsl(var(--_surface-color-bg));
+
+        border: calc(0.5 * var(--sys-spacing-track)) solid
+          hsla(var(--_surface-color-on), 30%);
+
+        font-size: calc(8 * var(--sys-spacing-track));
+        margin: calc(0.5 * var(--sys-spacing-track));
+        width: calc(12 * var(--sys-spacing-track));
+        height: calc(12 * var(--sys-spacing-track));
+
+        transition-property: border-color;
+        transition-duration: var(--sys-motion-duration-small);
+        transition-timing-function: var(--sys-motion-easing-in-out);
+      }
+
+      :host([selected]) {
+        border: calc(0.5 * var(--sys-spacing-track)) solid
+          hsla(var(--_surface-color-on), 100%);
+      }
+    `,
+  ];
+
+  @property({type: String})
+    value: Gender = 'unknown';
+
+  @property({type: Boolean, reflect: true})
+    selected = false;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    this.setAttribute('stated', '');
+    this.setAttribute('elevated', '3');
+  }
+
+  override render(): LitRenderType {
+    super.render();
+
+    const icon = this.value === 'male' ? maleIcon : femaleIcon;
+
+    if (this.value === 'male' || this.value === 'female') {
+      return html`<gecut-icon .svgContent=${icon}></gecut-icon>`;
+    }
+
+    return nothing;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'gecut-gender-card': GenderCard;
+  }
+}
