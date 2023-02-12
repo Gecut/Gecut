cdba099f2e886a8fd303faf1b8265d962c750830
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-13 03:05:25 +0330

    feat(ui|agah-pwa|types|route): remove icon

--- uniquely/ui/agah-pwa/src/types/route.ts
+++ uniquely/ui/agah-pwa/src/types/route.ts
@@ -2,7 +2,6 @@ import type {Route as alwatrRoute} from '@alwatr/router';
 import type {LitRenderType} from './lit-render.js';
 
 export type Route = {
-  icon: string | null;
   render: (route: alwatrRoute) => LitRenderType;
 };
 export type Routes = Record<string, Readonly<Route>>;
