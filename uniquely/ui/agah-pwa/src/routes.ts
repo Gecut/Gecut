9df8cb16081889787d9208d5e774f902967f0783
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-13 03:05:07 +0330

    feat(ui|agah-pwa|route): define support

--- uniquely/ui/agah-pwa/src/routes.ts
+++ uniquely/ui/agah-pwa/src/routes.ts
@@ -5,33 +5,31 @@ import './pages/page-sign-up';
 import './pages/page-sign-up-details';
 import './pages/page-user';
 import './pages/page-sans-list';
+import './pages/page-support';
 import './components/color-palette';
 
 import type {Routes} from './types/route.js';
 
 const routes: Routes = {
   'home': {
-    icon: null,
     render: () => html`<page-home></page-home>`,
   },
   'user': {
-    icon: null,
     render: () => html`<page-user></page-user>`,
   },
+  'support': {
+    render: () => html`<page-support></page-support>`,
+  },
   'sans-list': {
-    icon: null,
     render: () => html`<page-sans-list></page-sans-list>`,
   },
   'sign-up': {
-    icon: null,
     render: () => html`<page-sign-up></page-sign-up>`,
   },
   'sign-up-details': {
-    icon: null,
     render: () => html`<page-sign-up-details></page-sign-up-details>`,
   },
   'colors': {
-    icon: null,
     render: () => html`<color-palette></color-palette>`,
   },
 };
