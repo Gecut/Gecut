c495225af2acf6522cf0115997503eeee04f29d3
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-21 23:27:50 +0330

    feat: deploy

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+projectRoot = '~/Gecut/'
+storageServerRoot = '~/Gecut/services/storage-server'
+serverRoot = '~/Gecut/server/agah-api'
+pwaRoot = '~/Gecut/uniquely/ui/agah-pwa/'
+pwaDist = '/var/www/html'
+
+git pull --prune --progress --autostash --rebase
+
+yarn build
+
+cd "$storageServerRoot"
+yarn build
+pm2 start dist/index.mjs
+
+cd "$serverRoot"
+yarn build
+pm2 start dist/index.mjs
+
+cd "$pwaRoot"
+yarn build
+sudo rm -rf "$pwaDist/*"
+cp build/* "$pwaDist"
+
+sudo nginx -t
+sudo systemctl restart nginx
