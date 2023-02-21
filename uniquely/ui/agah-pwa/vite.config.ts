65a816049678ee79221f2e037730fff6ef6435a1
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-21 23:58:44 +0330

    fix(agah-pwa|sw): register sw

--- uniquely/ui/agah-pwa/vite.config.ts
+++ uniquely/ui/agah-pwa/vite.config.ts
@@ -72,6 +72,7 @@ export default defineConfig({
       mode: 'production',
       outDir: DIST_PATH,
       registerType: 'autoUpdate',
+      injectRegister: 'inline',
       useCredentials: true,
     }),
   ],
