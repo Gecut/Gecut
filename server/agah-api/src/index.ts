64ff6e84d7faceab917a8eb72eb963d3a0c85012
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-02-22 01:54:03 +0330

    feat(agah-api|sans): delete route

--- server/agah-api/src/index.ts
+++ server/agah-api/src/index.ts
@@ -5,6 +5,7 @@ import './routes/user/get.js';
 import './routes/auth/sign-up.js';
 import './routes/auth/sign-in.js';
 import './routes/admin/sans/post.js';
+import './routes/admin/sans/delete.js';
 import './routes/admin/user/post.js';
 import './routes/admin/user/get.js';
 import {logger} from './config.js';
